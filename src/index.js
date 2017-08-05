f703fb6862fa2fcadf503c5d44730bf578c152f7
Alexandru Ghiura <ghalex@gmail.com>
2017-08-05 12:53:10 +0300

    Update main import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Button from 'Button'
 import ButtonGroup from 'ButtonGroup'
 import Icon from 'Icon'
 import Image from 'Image'
+import Subtitle from 'Subtitle'
+import Title from 'Title'
 
 import theme from './theme'
 
@@ -12,5 +14,7 @@ export {
   ButtonGroup,
   Icon,
   Image,
+  Subtitle,
+  Title,
   theme
 }
